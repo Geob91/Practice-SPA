e0fcf25eeb355e42d40986f518a68e233423288b
George Blezard <gblezard91@gmail.com>
2019-12-30 21:31:02 -0600

    add netlify data

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 //accepts css selector as string
 
 const form = document.querySelector("form");
+
+//listen for an event
+//use a call back function when event occurs
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log("I am submitted");
+});
 console.log(form);
