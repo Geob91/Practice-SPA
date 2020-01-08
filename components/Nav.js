379e4c86c7cc02c8a1c61c778df7da98404f7d04
George Blezard <gblezard91@gmail.com>
2020-01-07 20:51:46 -0600

    add starter view files

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,5 @@
 function createListHTML(links) {
-  return links
-    .map(link => `<li class ="button"><a href ="/">${link}</li>`)
-    .join("");
+  return links.map(link => `<li class ="button">${link}</li>`).join("");
 }
 
 export default st => {
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import { Header, Nav, Main, Footer } from "./components";
 
+import * as state from "./store";
+
 import capitalize from "lodash.capitalize";
 
 console.log(capitalize("hello"));
 
 //console.log(Header, Footer, Main, Nav);
-
-const state = {
-  Home: {
-    heading: "Home"
-  },
-  Form: {
-    heading: "Form"
-  },
-  Blog: {
-    heading: "Blog"
-  },
-  Gallery: {
-    heading: "Gallery"
-  },
-  Links: ["Home", "Form", "Blog", "Gallery"]
-};
 
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
@@ -29,7 +15,7 @@ ${Nav(state.Links)}
 ${Main(st)}
 ${Footer(st)}`;
 
-  document.querySelectorAll("nav a").forEach(link => {
+  document.querySelectorAll("nav ul").forEach(link => {
     link.addEventListener("click", function(event) {
       event.preventDefault();
       render(state[event.target.textContent]);
